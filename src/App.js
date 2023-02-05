0415606bba41cb0983dbffab4cba8d96a2f75d96
MIROSLAVSH <sharkovskiymiroslav@gmail.com>
2023-02-05 12:19:32 +0300

    docs: change name

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h2>Счетчик:</h2>
+        <h2>Total:</h2>
         <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <button className="minus">- Minus</button>
+        <button className="plus">Plus +</button>
       </div>
     </div>
   );
